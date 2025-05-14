b6469962c1222adeacb7966dc3f55bbd8dcd0730
ObakengLetsogo <61384076+ObakengLetsogo@users.noreply.github.com>
2025-05-14 08:49:32 +0200

    Add files via upload
    uplaoded door2doorza-api files from sprin initialyzer

---
+++ src/main/java/com/door2doorza/door2doorza_api/Door2doorzaApiApplication.java
@@ -0,0 +1,13 @@
+package com.door2doorza.door2doorza_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Door2doorzaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Door2doorzaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/door2doorza/door2doorza_api/Door2doorzaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.door2doorza.door2doorza_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Door2doorzaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
